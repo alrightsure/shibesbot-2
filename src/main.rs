aa51c208dd776b2aac74844558da8d193b89c898
alrightsure <cullen0880@gmail.com>
2022-12-04 15:49:10 -0500

    Finished shibesbot 2.0

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use dotenv::dotenv;
+use std::env;
+
+use serenity::async_trait;
+use serenity::prelude::*;
+use serenity::model::channel::Message;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{StandardFramework, CommandResult};
+
+use reqwest::Url;
+
+#[group]
+#[commands(ping,shibe)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("!"))
+        .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
+}
+
+#[command]
+async fn shibe(ctx: &Context, msg: &Message) -> CommandResult {
+    let url = env::var("API_URL").expect("url");
+    let url = Url::parse(&*url)?;
+    let res = reqwest::get(url).await?.json::<Vec<String>>().await?;
+
+    msg.channel_id.say(ctx, &res[0]).await?;
+    
+    Ok(())
+}
